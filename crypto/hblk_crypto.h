b8dc6530d4796c775081c5e971dd52606ba679ac
Arsene Karuretwa <AKaruretwa@sisi.local>
2026-06-05 10:31:10 +0200

    betty adjusted

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -5,6 +5,7 @@
 #include <stddef.h>
 #include <openssl/sha.h>
 
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
+uint8_t *sha256(int8_t const *s, size_t len,
+		uint8_t digest[SHA256_DIGEST_LENGTH]);
 
 #endif /* HBLK_CRYPTO_H */
